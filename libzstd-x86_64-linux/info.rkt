3eb6390e3d459097a0566b32b10477281931abb0
Bogdan Popa <bogdan@defn.io>
2023-06-29 21:37:36 +0300

    build: fixup linux path

--- libzstd-x86_64-linux/info.rkt
+++ libzstd-x86_64-linux/info.rkt
@@ -3,5 +3,5 @@
 (define license 'BSD-3-Clause)
 (define version "1.5.5")
 (define deps '("base"))
-(define copy-foreign-libs '("libzstd.dylib"))
+(define copy-foreign-libs '("libzstd.so"))
 (define install-platform #rx"x86_64-linux")
