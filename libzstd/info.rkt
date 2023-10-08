b3701f8dc7376592d584d8d07b0a87b6507af603
Bogdan Popa <bogdan@defn.io>
2023-10-08 15:06:45 +0300

    build: fix platform specifiers on windows

--- libzstd/info.rkt
+++ libzstd/info.rkt
@@ -8,5 +8,5 @@
     ["libzstd-aarch64-macosx" #:platform #rx"aarch64-macosx"]
     ["libzstd-x86_64-linux" #:platform #rx"x86_64-linux"]
     ["libzstd-x86_64-macosx" #:platform #rx"x86_64-macosx"]
-    ["libzstd-i386-win32" #:platform #rx"i386.win32"]
-    ["libzstd-x86_64-win32" #:platform #rx"x86_64.win32"]))
+    ["libzstd-i386-win32" #:platform #rx"win32.i386"]
+    ["libzstd-x86_64-win32" #:platform #rx"win32.x86_64"]))
